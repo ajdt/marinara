a50a89958974a170fba215ba1e98f5dfd4712a6a
ajdt <ajdt@uw.edu>
2016-07-30 16:01:19 -0700

    added test dependencies to module-level build.gradle file

---
+++ app/src/androidTest/java/com/itsaunixsystem/marinara/TimerButtonTest.java
@@ -0,0 +1,27 @@
+package com.itsaunixsystem.marinara;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+
+import android.support.test.runner.AndroidJUnit4;
+
+/**
+ * @author: ajdt on 7/30/16.
+ * @description: test that timer button UI works as expected
+ */
+
+//@RunWith(AndroidJUnit4.class)
+public class TimerButtonTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
