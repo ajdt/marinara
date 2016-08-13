33d428b0a2aed8c762bcfabac05bb03d231193a7
ajdt <ajdt@uw.edu>
2016-08-13 09:04:16 -0700

    Removed all options menu logic from AddTaskActivity and NewTaskDialogActivity

--- app/src/main/java/com/itsaunixsystem/marinara/NewTaskDialogActivity.java
+++ app/src/main/java/com/itsaunixsystem/marinara/NewTaskDialogActivity.java
@@ -18,29 +18,6 @@ public class NewTaskDialogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_task_dialog);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_new_task_dialog, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
     public void onOkButtonClicked(View view) {
         // get task name
         String new_task_name = ((EditText)findViewById(R.id.task_dialog_edit_text))
